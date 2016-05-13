9cbe5357d51ed35f60874f29a213749f32697c70
Patrick Pigmans <ppigmans@hotmail.com>
2016-05-13 10:20:01 +0200

    Update ipconfig.sh

--- ipconfig.sh
+++ ipconfig.sh
@@ -1,11 +1,13 @@
-PS3="Hoe zit uw aangesloten op het netwerk (1-3):" 
-select name in Bekabeld Wifi afsluiten
+PS3="Hoe zit u op het netwerk aangesloten, Via (1-3):" 
+select name in Kabel WIFI Afsluiten
 do 
  	break 
 done 
-echo "You picked $name."
 
-if [ "$name" = "Bekabeld" ]; then
+if [ "$name" = "Kabel" ]; then
+echo
+echo "U zit aangesloten via $name"
+echo
 echo "Uw ip adress is"
 echo
 ipconfig=$( ifconfig eth1 | head -n 2 | sed 'N;s/\n/ /;N;s/\n/ /' | awk '{ print $7}' | cut -d: -f2 )
@@ -17,12 +19,22 @@ exit 1
 fi
 
 if [ "$name" = "Wifi" ]; then
+echo
+echo "U zit aangesloten via $name"
+echo
 echo "Uw ip adress is"
 echo
 ipconfig=$( ifconfig wlan1 | head -n 2 | sed 'N;s/\n/ /;N;s/\n/ /' | awk '{ print $7}' | cut -d: -f2 )
 echo "${ipconfig}"
 echo
 echo "Druk op enter om door te gaan..."
+read enter
+exit 1
+fi
+
+if [ "$name" = "Afsluiten" ]; then
+echo
+echo "Druk op enter om af te sluiten..."
 read enter
 exit 1
 fi
